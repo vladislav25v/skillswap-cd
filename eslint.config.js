63625e577862f6791411ada06e3e31ce37c844f7
Mikhail Zakharov <onestent@gmail.com>
2026-03-06 19:53:45 +0300

    fix: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,22 +2,30 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tseslint from '@typescript-eslint/eslint-plugin'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
+    ignores: ["node_modules/**", "dist/**"],
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      'plugin:prettier/recommended' 
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    plugins: {
+      prettier: tseslint, 
+    },
+    rules: {
+      'prettier/prettier': ['warn', { semi: true }], 
     },
   },
 ])
