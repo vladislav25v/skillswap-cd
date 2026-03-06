c5ee8bb75259aa0e90ad3b2cc1afad7489db5bf3
Mikhail Zakharov <onestent@gmail.com>
2026-03-06 18:55:35 +0300

    fix: test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Button from './shared/ui/Button/Button';
+
 function App() {
   return (
     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
       <h1>Проектный месяц</h1>
+      <Button></Button>
     </div>
   );
 }
