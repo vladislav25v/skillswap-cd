51033259da9ce73117da829a6817764e87d79603
mrnobody <corvus007@yandex.ru>
2026-03-09 21:45:30 +0300

    fix: удаляет подключение шрифта Roboto c жирностью 600
    в макете используется Regular (400) и Medium (500)

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App.tsx';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/600.css';
 import '@fontsource/jost/500.css';
 
 createRoot(document.getElementById('root')!).render(
