0c40cee570f491026ef1650b67e665f4ca8b9d87
mrnobody <corvus007@yandex.ru>
2026-03-09 21:42:46 +0300

    fix: добавляет шрифт `Jost`

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/600.css';
+import '@fontsource/jost/500.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
