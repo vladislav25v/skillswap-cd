f107dfab3ffd34d70028b447acf9ad6f6a1df4e0
Vladislav <vlad-ski-96borsh@ya.ru>
2026-03-10 02:44:04 +0900

    refactor: normalize mock data structure

---
+++ src/entities/skill/types.ts
@@ -0,0 +1,8 @@
+export interface Skill {
+  id: number;
+  userId: number;
+  title: string;
+  subcategoryId: number;
+  description: string;
+  image: string;
+}
---
+++ src/entities/user/types.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: number;
+  name: string;
+  cityId: number;
+  age: number;
+  photo: string;
+  registeredAt: string;
+  likes: number;
+  desiredSubcategoryIds: number[];
+}
