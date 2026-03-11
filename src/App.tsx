9f06153076ec52c8afe758589912f633ebb4a4bd
Vladislav <vlad-ski-96borsh@ya.ru>
2026-03-11 23:16:06 +0900

    refactor: global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,3 @@
-import Button from './shared/ui/Button/Button';
-import Nav from './shared/ui/Nav/Nav';
-import AvatarTest from './shared/ui/Avatar/test/Avatar.test';
-
-function App() {
-  const handleClick = () => {
-    console.log('Кнопка нажата!');
-  };
-
-  return (
-    <>
-      {/* Основной интерфейс */}
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '16px' }}>
-        <Nav />
-        <h1>Проектный месяц</h1>
-        <Button variant="secondary">Войти</Button>
-        <Button variant="primary" onClick={handleClick}>
-          Зарегистрироваться
-        </Button>
-      </div>
-
-      {/* Тесты Avatar */}
-      <AvatarTest />
-    </>
-  );
-}
+function App() {}
 
 export default App;
