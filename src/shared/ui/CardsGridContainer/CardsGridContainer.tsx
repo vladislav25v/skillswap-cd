4c8de3ee3c4d40f75dd769e6adc0cded76160c89
Timofey Zakovriashin <timurowtimur@gmail.com>
2026-03-12 17:51:17 +0300

    fix: исправлен компонент CardsGridContainer с добавлением стилей

--- src/shared/ui/CardsGridContainer/CardsGridContainer.tsx
+++ src/shared/ui/CardsGridContainer/CardsGridContainer.tsx
@@ -2,8 +2,9 @@ import styles from './CardsGridContainer.module.css';
 
 type CardsGridContainerProps = {
   children: React.ReactNode;
+  className?: string;
 };
 
-export function CardsGridContainer({ children }: CardsGridContainerProps) {
-  return <div className={styles.grid}>{children}</div>;
+export function CardsGridContainer({ children, className = '' }: CardsGridContainerProps) {
+  return <div className={`${styles.grid} ${className}`}>{children}</div>;
 }
