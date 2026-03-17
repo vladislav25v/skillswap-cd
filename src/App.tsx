2f307657ef16170ab128684cfca4711009f32dd0
Poll <stratum90@mail.ru>
2026-03-17 20:09:14 +0300

    feat: add-RegPageStep1

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
-function App() {}
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ResPageStep1 from './pages/ResPageStep1';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register/step1" element={<ResPageStep1 />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
