f85d9f8a30ef765b94ee57e2890cecc84e5f7f64
Poll <stratum90@mail.ru>
2026-03-20 19:57:00 +0300

    feat: add-MainLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
-<<<<<<< feature/skill-page
 function App() {
   return null;
-=======
-import { RouterProvider } from 'react-router-dom';
-import { router } from '@/app/router/router';
-
-function App() {
-  return <RouterProvider router={router} />;
->>>>>>> develop
 }
 
 export default App;
---
+++ src/widgets/AuthLayout/index.ts
@@ -0,0 +1,2 @@
+export { AuthLayout } from './AuthLayout';
+export type { AuthLayoutProps } from './AuthLayout';
