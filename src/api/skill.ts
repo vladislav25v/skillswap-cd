6e27cb642b91b21f0ed6808f5a501cd781088620
Timofey Zakovriashin <timurowtimur@gmail.com>
2026-03-23 20:46:38 +0300

    fix: исправлены метода и экспорты API

--- src/api/skill.ts
+++ src/api/skill.ts
@@ -1,6 +1,6 @@
 import type { Skill } from '@/entities/skill/types';
 import { request } from '@/api/request';
 
-export function getSkills() {
-  return request<Skill[]>('/skills');
-}
+export const SKILL_API_PATH = '/skills';
+
+export const getSkills = async (): Promise<Skill[]> => request<Skill[]>(SKILL_API_PATH);
