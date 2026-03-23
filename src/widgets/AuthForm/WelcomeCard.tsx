825a02b4b3ed95fc4f143cb3441328c15ed8307c
Poll <stratum90@mail.ru>
2026-03-23 19:46:01 +0300

    refactor: recycling LoginPage

---
+++ src/widgets/AuthForm/WelcomeCard.tsx
@@ -0,0 +1,23 @@
+import Title from '@/shared/ui/Title';
+import styles from './WelcomeCard.module.scss';
+
+interface WelcomeCardProps {
+  title: string;
+  text: string;
+  image: string;
+  imageAlt: string;
+}
+
+export const WelcomeCard: React.FC<WelcomeCardProps> = ({ title, text, image, imageAlt }) => {
+  return (
+    <div className={styles.welcome}>
+      <img src={image} width={300} height={300} alt={imageAlt} className={styles.bulb} />
+      <div className={styles.welcomeTextBlock}>
+        <Title tag="h3" looksLike="h3" className={styles.welcomeTitle}>
+          {title}
+        </Title>
+        <p className={styles.welcomeText}>{text}</p>
+      </div>
+    </div>
+  );
+};
