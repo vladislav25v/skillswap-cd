f2ccccf42424b6cd18c5d5c8c37fe5c166c1fcc6
Kinza <kira-kazan1997@yandex.ru>
2026-03-26 16:47:32 +0300

    feat: update Header and Footer components

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -4,7 +4,7 @@ import { PrivateLayout } from '@/app/layouts/private-layout';
 import LoginPage from '@/pages/LoginPage/LoginPage';
 import RegPageStep1 from '@/pages/RegisterPage/RegisterPageStep1';
 import { CatalogPage } from '@/pages/CatalogPage';
-import { SkillPage } from '@/pages/skillPage/SkillPage';
+import { SkillPage } from '@/pages/SkillPage/SkillPage';
 import ProfilePage from '@/pages/ProfilePage';
 import { ErrorPage404 } from '@/pages/ErrorPage404';
 import { ErrorPage500 } from '@/pages/ErrorPage500';
